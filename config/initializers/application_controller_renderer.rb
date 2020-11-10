563dd018ea8ad616165356dc81829013cae35c1a
okushogg <okusho721@gmail.com>
2020-11-10 07:23:32 +0000

    [Start]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '86a65c47dffe45ec43d52752ae774a820f9435f0db1af171e87c6f74ec94c0594e0a3fc1fb0a9a7a1651123bf93da8eb717226a9469a828af4eff4b734a6c19f'
